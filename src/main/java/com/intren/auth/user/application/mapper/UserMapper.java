058cfb93c0355489e8e8ec86db154bae9a5e712c
AnJungGeon <gunn0128@gmail.com>
2025-05-13 23:42:31 +0900

    feat[#1]: UserMapper 생성 - 회원가입 요청 DTO를 User 엔티티로 변환하는 매핑 메서드 추가 - User 엔티티를 회원가입 응답 DTO로 변환하는 메서드 추가

---
+++ src/main/java/com/intren/auth/user/application/mapper/UserMapper.java
@@ -0,0 +1,27 @@
+package com.intren.auth.user.application.mapper;
+
+import com.intren.auth.user.application.dto.request.SignupRequestDto;
+import com.intren.auth.user.application.dto.response.RoleResponseDto;
+import com.intren.auth.user.application.dto.response.SignupResponseDto;
+import com.intren.auth.user.domain.model.User;
+
+import java.util.List;
+
+public class UserMapper {
+
+    public static User toEntity(SignupRequestDto requestDto) {
+        return User.of(
+                requestDto.getUsername(),
+                requestDto.getPassword(),
+                requestDto.getNickname()
+        );
+    }
+
+    public static SignupResponseDto toResponseDto(User user) {
+        return SignupResponseDto.of(
+                user.getUsername(),
+                user.getNickname(),
+                List.of(new RoleResponseDto(user.getRole()))
+        );
+    }
+}
