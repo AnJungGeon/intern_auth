7935071fe513860fe90aed0f2758ffec17c51867
AnJungGeon <gunn0128@gmail.com>
2025-05-13 23:44:40 +0900

    feat[#1]: UserService 생성 및 회원가입 서비스 메서드 추가 - 회원가입 로직을 처리하는 서비스 메서드를 작성했습니다

---
+++ src/main/java/com/intren/auth/user/application/service/UserServiceImpl.java
@@ -0,0 +1,29 @@
+package com.intren.auth.user.application.service;
+
+import com.intren.auth.user.application.dto.request.SignupRequestDto;
+import com.intren.auth.user.application.dto.response.SignupResponseDto;
+import com.intren.auth.user.application.mapper.UserMapper;
+import com.intren.auth.user.application.validator.DuplicateUserValidator;
+import com.intren.auth.user.domain.model.User;
+import com.intren.auth.user.domain.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@RequiredArgsConstructor
+public class UserServiceImpl implements UserService {
+
+    private final UserRepository userRepository;
+    private final DuplicateUserValidator duplicateUserValidator;
+
+    @Override
+    @Transactional
+    public SignupResponseDto signup(SignupRequestDto requestDto) {
+        duplicateUserValidator.validateDuplicateUser(requestDto.getUsername());
+        User user = UserMapper.toEntity(requestDto);
+        userRepository.save(user);
+
+        return UserMapper.toResponseDto(user);
+    }
+}
