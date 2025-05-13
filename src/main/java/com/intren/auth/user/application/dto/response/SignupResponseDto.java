a60038fa55aeb94cf9cfa1804759ebf3badc0a15
AnJungGeon <gunn0128@gmail.com>
2025-05-13 23:39:14 +0900

    feat[#1]: 회원가입 응답 dto 생성 - 회원가입 응답 dto 생성했습니다

---
+++ src/main/java/com/intren/auth/user/application/dto/response/SignupResponseDto.java
@@ -0,0 +1,32 @@
+package com.intren.auth.user.application.dto.response;
+
+import com.intren.auth.user.domain.model.User;
+import lombok.Getter;
+
+import java.util.List;
+
+@Getter
+public class SignupResponseDto {
+
+    private final String username;
+    private final String nickname;
+    private final List<RoleResponseDto> roles;
+
+    public SignupResponseDto(
+            String username,
+            String nickname,
+            List<RoleResponseDto> roles
+    ){
+        this.username = username;
+        this.nickname = nickname;
+        this.roles = roles;
+    }
+
+    public static SignupResponseDto of(
+            String username,
+            String nickname,
+            List<RoleResponseDto> roles
+    ){
+        return new SignupResponseDto(username, nickname, roles);
+    }
+}
