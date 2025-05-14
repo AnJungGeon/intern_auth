4538f15cf05e19d5f3768f3e74c6302d6c66600f
AnJungGeon <gunn0128@gmail.com>
2025-05-14 21:18:49 +0900

    feat[#9] : 권한 변경 요청 메서드 추가 - User 컨트롤러에 권한 변경 요청 메서드 추가 했습니다

--- src/main/java/com/intren/auth/user/presentation/UserController.java
+++ src/main/java/com/intren/auth/user/presentation/UserController.java
@@ -2,14 +2,14 @@ package com.intren.auth.user.presentation;
 
 import com.intren.auth.user.application.dto.request.SignupRequestDto;
 import com.intren.auth.user.application.dto.response.SignupResponseDto;
+import com.intren.auth.user.application.dto.response.UpdateUserRolesResponseDto;
 import com.intren.auth.user.application.service.UserService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.security.access.annotation.Secured;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
@@ -24,5 +24,14 @@ public class UserController {
         SignupResponseDto response = userService.signup(requestDto);
 
         return ResponseEntity.status(HttpStatus.CREATED).body(response);
+    }
+
+
+    @Secured("ADMIN")
+    @PatchMapping("/admin/users/{userId}/roles")
+    public ResponseEntity<UpdateUserRolesResponseDto> updateUserRoles(
+            @Valid @PathVariable Long userId){
+        UpdateUserRolesResponseDto responseDto = userService.updateUserRoles(userId);
+        return ResponseEntity.ok(responseDto);
     }
 }
