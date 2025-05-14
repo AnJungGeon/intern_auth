2503f74387f5db07c72c9bdc01b0b5c361095551
AnJungGeon <gunn0128@gmail.com>
2025-05-15 02:13:49 +0900

    feat[#11] : 회원가입 요청 dto 생성자 추가

--- src/main/java/com/intren/auth/user/application/dto/request/SignupRequestDto.java
+++ src/main/java/com/intren/auth/user/application/dto/request/SignupRequestDto.java
@@ -15,6 +15,10 @@ public class SignupRequestDto {
     @NotBlank(message = "닉네임은 필수 입니다")
     private String nickname;
 
-
+    public SignupRequestDto(String username, String password, String nickname) {
+        this.username = username;
+        this.password = password;
+        this.nickname = nickname;
+    }
 
 }
