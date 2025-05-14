aebac2babaf9e4d5bdd92920aed5c8c8e3552cf8
AnJungGeon <gunn0128@gmail.com>
2025-05-14 16:20:08 +0900

    feat[#5] : 로그인 유효성 검증 클래스 생성 - 사용자 존재 여부 및 비밀번호 일치 여부 검증 - 비밀번호 불일치 시 AuthException 발생

---
+++ src/main/java/com/intren/auth/authentication/application/vaildator/LoginValidator.java
@@ -0,0 +1,26 @@
+package com.intren.auth.authentication.application.vaildator;
+
+import com.intren.auth.authentication.domain.exception.AuthErrorCode;
+import com.intren.auth.authentication.domain.exception.AuthException;
+import com.intren.auth.authentication.infrastructure.password.PasswordEncoderUtil;
+import com.intren.auth.user.domain.model.User;
+import com.intren.auth.user.domain.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+public class LoginValidator {
+
+    private final UserRepository userRepository;
+    private final PasswordEncoderUtil passwordEncoderUtil;
+
+    public User validateLogin(String username, String password) {
+        User user = userRepository.findByUsernmae(username);
+
+        if(!passwordEncoderUtil.isMatch(password, user.getPassword())){
+            throw new AuthException(AuthErrorCode.INVALID_CREDENTIALS);
+        }
+        return user;
+    }
+}
