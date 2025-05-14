a55344171724a41bbcd508bd28afd1dd8fd11b95
AnJungGeon <gunn0128@gmail.com>
2025-05-14 21:20:18 +0900

    feat[#9] : 시큐리티 예외 처리 추가 - 시큐리티에 적용하는 커스텀 예외 추가했습니다 - 토큰 관련 검증 에러 입니다.

---
+++ src/main/java/com/intren/auth/security/exception/CustomAccessDeniedHandler.java
@@ -0,0 +1,46 @@
+package com.intren.auth.security.exception;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import com.intren.auth.common.exception.ErrorResponse;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.security.access.AccessDeniedException;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.access.AccessDeniedHandler;
+
+import java.io.IOException;
+
+public class CustomAccessDeniedHandler implements AccessDeniedHandler {
+
+    private final ObjectMapper objectMapper = new ObjectMapper();
+
+    @Override
+    public void handle(
+            HttpServletRequest request,
+            HttpServletResponse response,
+            AccessDeniedException accessDeniedException) throws IOException, ServletException {
+        String message = getAccessDeniedMessage();
+
+        ErrorResponse errorResponse = new ErrorResponse(SecurityErrorCode.ACCESS_DENIED, message);
+        response.setStatus(SecurityErrorCode.ACCESS_DENIED.getHttpStatus().value());
+        response.setContentType("application/json");
+        response.setCharacterEncoding("UTF-8");
+        objectMapper.writeValue(response.getWriter(), errorResponse);
+    }
+
+    private String getAccessDeniedMessage() {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+
+        if(authentication != null&& authentication.isAuthenticated()) {
+            boolean isAdmin = authentication.getAuthorities().contains(new SimpleGrantedAuthority("ROLE_ADMIN"));
+            if(!isAdmin) {
+                return "관리자 권한이 필요한 요청입니다. 접근 권한이 없습니다";
+            }
+        }
+        return "접근 권한이 없습니다.";
+    }
+}
